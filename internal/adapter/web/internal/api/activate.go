0c05a96938e4139d4209eae93aa72b7fd907b623
Polyscone <polyscone@gmail.com>
2023-04-18 20:35:22 +0900

    Only consume activation token after verification

--- internal/adapter/web/internal/api/activate.go
+++ internal/adapter/web/internal/api/activate.go
@@ -19,15 +19,23 @@ func (api *API) accountActivatePost(w http.ResponseWriter, r *http.Request) {
 
 	ctx := r.Context()
 
-	err := api.tokens.ConsumeActivationToken(ctx, input.Token)
-	if writeError(w, r, errors.Tracef(err)) {
-		return
-	}
-
 	cmd := account.Activate{
 		Email:    input.Email,
 		Password: input.Password,
 	}
+	err := cmd.Validate(ctx)
+	if writeError(w, r, errors.Tracef(err)) {
+		return
+	}
+
+	// Only consume after manual command validation, but before execution
+	// This way the token will only be consumed once we know there aren't any
+	// input validation or authorisation errors
+	err = api.tokens.ConsumeActivationToken(ctx, input.Token)
+	if writeError(w, r, errors.Tracef(err)) {
+		return
+	}
+
 	err = cmd.Execute(ctx, api.bus)
 	if writeError(w, r, errors.Tracef(err)) {
 		return
