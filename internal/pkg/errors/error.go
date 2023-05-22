e3cc549f185dc616214385eb9cd729e6b43d10e9
Polyscone <polyscone@gmail.com>
2023-05-22 16:30:35 +0900

    Re-export `errors.Join`

--- internal/pkg/errors/error.go
+++ internal/pkg/errors/error.go
@@ -2,22 +2,11 @@ package errors
 
 import "errors"
 
-// As wraps the standard library's errors.As function.
-func As(err error, target any) bool {
-	return errors.As(err, target)
-}
-
-// Is wraps the standard library's errors.Is function.
-func Is(err, target error) bool {
-	return errors.Is(err, target)
-}
-
-// Unwrap wraps the standard library's errors.Unwrap function.
-func Unwrap(err error) error {
-	return errors.Unwrap(err)
-}
-
-// New wraps the standard library's errors.New function.
-func New(text string) error {
-	return errors.New(text)
-}
+// Re-export standard library functions
+var (
+	As     = errors.As
+	Is     = errors.Is
+	Join   = errors.Join
+	New    = errors.New
+	Unwrap = errors.Unwrap
+)
