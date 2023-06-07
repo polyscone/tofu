8988f0f954414d5bd1571a906384e055b12ff586
Polyscone <polyscone@gmail.com>
2023-06-07 11:12:12 +0900

    Improve top bar styles

--- internal/adapter/web/passport/permissions.go
+++ internal/adapter/web/passport/permissions.go
@@ -2,12 +2,17 @@ package passport
 
 var PermissionGroups = []PermissionGroup{
 	{
-		Name: "Account",
+		Name: "Account roles",
 		Permissions: []Permission{
 			{Name: "account:roles:view", DisplayName: "View roles"},
 			{Name: "account:roles:create", DisplayName: "Create roles"},
 			{Name: "account:roles:edit", DisplayName: "Edit roles"},
 			{Name: "account:roles:delete", DisplayName: "Delete roles"},
+		},
+	},
+	{
+		Name: "Account users",
+		Permissions: []Permission{
 			{Name: "account:users:view", DisplayName: "View users"},
 			{Name: "account:users:edit", DisplayName: "Edit users"},
 		},
