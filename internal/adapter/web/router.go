51b3f9418c99621b89aef02a06d396f57cb8a04d
Polyscone <polyscone@gmail.com>
2023-07-04 10:38:46 +0900

    Move site routes into their own `site` package

--- internal/adapter/web/router.go
+++ internal/adapter/web/router.go
@@ -22,6 +22,10 @@ func NewRouter(tenant *handler.Tenant) http.Handler {
 	mux := http.NewServeMux()
 	h := handler.New(tenant)
 
+	// API routes should always be setup before site/PWA routes because routes
+	// for the site and PWA could include fallback routes for serving files
+	// which would prevent API routes from being run if they were setup after
+	// when the router finds a match in the order routes are declared
 	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", NewAPIRouter(h)))
 
 	switch tenant.Kind {
