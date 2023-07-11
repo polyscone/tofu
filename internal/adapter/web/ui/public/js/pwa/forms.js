a527b2ad29e9e822b1918be948877a7a0ac93d70
Polyscone <polyscone@gmail.com>
2023-07-11 12:26:15 +0900

    Abstract form inputs into a base input component

--- internal/adapter/web/ui/public/js/pwa/forms.js
+++ internal/adapter/web/ui/public/js/pwa/forms.js
@@ -1,78 +1,75 @@
+export const Input = {
+	view (vnode) {
+		if (!vnode.attrs.type) {
+			throw new Error("Form input requires a type")
+		}
+		if (!vnode.attrs.name) {
+			throw new Error("Form input requires a name")
+		}
+
+		const id = vnode.attrs.id || `form__${vnode.attrs.name}`
+
+		return [
+			vnode.attrs.label ? m("label", {
+				for: id,
+				class: vnode.attrs.required ? "required" : null,
+			}, vnode.attrs.label) : null,
+			m(`input[type=${vnode.attrs.type}]`, {
+				oninput: vnode.attrs.oninput,
+				value: vnode.attrs.value,
+				id: id,
+				required: vnode.attrs.required,
+				placeholder: vnode.attrs.placeholder,
+				autocomplete: vnode.attrs.autocomplete,
+				pattern: vnode.attrs.pattern,
+				minlength: vnode.attrs.minlength,
+				maxlength: vnode.attrs.maxlength,
+				class: vnode.attrs.error ? "invalid" : null,
+			}),
+			m("p.error", vnode.attrs.error),
+		]
+	},
+}
+
 export const EmailInput = {
-	view: vnode => [
-		vnode.attrs.label ? m("label", {
-			for: vnode.attrs.id,
-			class: vnode.attrs.required ? "required" : null,
-		}, vnode.attrs.label) : null,
-		m("input[type=email]", {
-			oninput: vnode.attrs.oninput,
-			value: vnode.attrs.value,
-			id: vnode.attrs.id,
-			required: vnode.attrs.required,
-			placeholder: "email@example.com",
-			autocomplete: "email",
-			pattern: "^[a-zA-Z0-9+._~\\-]+@[a-zA-Z0-9+._~\\-]+(\.[a-zA-Z0-9+._~\\-]+)+$",
-			maxlength: 100,
-		}),
-	]
+	view: vnode => m(Input, {
+		...vnode.attrs,
+		type: "email",
+		placeholder: "email@example.com",
+		pattern: "^[a-zA-Z0-9+._~\\-]+@[a-zA-Z0-9+._~\\-]+(\.[a-zA-Z0-9+._~\\-]+)+$",
+		maxlength: 100,
+	}),
 }
 
 export const PasswordInput = {
-	view: vnode => [
-		vnode.attrs.label ? m("label", {
-			for: vnode.attrs.id,
-			class: vnode.attrs.required ? "required" : null,
-		}, vnode.attrs.label) : null,
-		m("input[type=password]", {
-			oninput: vnode.attrs.oninput,
-			value: vnode.attrs.value,
-			id: vnode.attrs.id,
-			required: vnode.attrs.required,
-			placeholder: "password",
-			autocomplete: vnode.attrs.autocomplete,
-			pattern: "^.+$",
-			minlength: 8,
-			maxlength: 1000,
-		}),
-	]
+	view: vnode => m(Input, {
+		...vnode.attrs,
+		type: "password",
+		placeholder: "password",
+		pattern: "^.+$",
+		minlength: 8,
+		maxlength: 1000,
+	}),
 }
 
 export const TOTPInput = {
-	view: vnode => [
-		vnode.attrs.label ? m("label", {
-			for: vnode.attrs.id,
-			class: vnode.attrs.required ? "required" : null,
-		}, vnode.attrs.label) : null,
-		m("input[type=text]", {
-			oninput: vnode.attrs.oninput,
-			value: vnode.attrs.value,
-			id: vnode.attrs.id,
-			required: vnode.attrs.required,
-			placeholder: "123456",
-			autocomplete: vnode.attrs.autocomplete,
-			pattern: "^\\d+$",
-			minlength: 6,
-			maxlength: 6,
-		}),
-	]
+	view: vnode => m(Input, {
+		...vnode.attrs,
+		type: "text",
+		placeholder: "123456",
+		pattern: "^\\d+$",
+		minlength: 6,
+		maxlength: 6,
+	}),
 }
 
 export const RecoveryCodeInput = {
-	view: vnode => [
-		vnode.attrs.label ? m("label", {
-			for: vnode.attrs.id,
-			class: vnode.attrs.required ? "required" : null,
-		}, vnode.attrs.label) : null,
-		m("input[type=text]", {
-			oninput: vnode.attrs.oninput,
-			value: vnode.attrs.value,
-			id: vnode.attrs.id,
-			required: vnode.attrs.required,
-			placeholder: "ABCDEFGHIJKLM",
-			autocomplete: "off",
-			pattern: "^[A-Z2-7]+$",
-			minlength: 13,
-			maxlength: 13,
-		}),
-	]
+	view: vnode => m(Input, {
+		...vnode.attrs,
+		type: "text",
+		placeholder: "ABCDEFGHIJKLM",
+		pattern: "^[A-Z2-7]+$",
+		minlength: 13,
+		maxlength: 13,
+	}),
 }
