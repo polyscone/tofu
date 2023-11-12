78d0619b338fb750bbfb3ca00501eeb03a88f15b
Polyscone <polyscone@gmail.com>
2023-11-12 15:16:17 +0900

    Take HTTP connection hijacking into account when recording metrics

--- internal/pkg/http/middleware/metrics.go
+++ internal/pkg/http/middleware/metrics.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	"bufio"
 	"expvar"
 	"io"
+	"net"
 	"net/http"
 	"strconv"
 	"sync"
@@ -65,16 +67,29 @@ func Metrics(metrics *expvar.Map, keys ...string) Middleware {
 				rw.statusCode = http.StatusOK
 			}
 
+			// The end time is used to estimate the amount of time spent
+			// inside of HTTP handler functions
+			//
+			// If the connection was hijacked then we just assume the end
+			// of the handler was at the time of the hijacking, since
+			// the handler function could still be running in an infinite
+			// loop whilst the TCP connection is being used for other purposes
+			//
+			// If there was no first write time recorded then we assume it's
+			// the hijacked time, if there is one, or the current time
 			end := time.Now()
-			if rw.end.IsZero() {
-				rw.end = end
+			if !rw.hijackedAt.IsZero() {
+				end = rw.hijackedAt
+			}
+			if rw.firstWriteAt.IsZero() {
+				rw.firstWriteAt = end
 			}
 
 			for i, key := range keys {
 				statusCodeMaps[i].Add(strconv.Itoa(rw.statusCode), 1)
 
 				metrics.Add(key+".totalResponsesSent", 1)
-				metrics.Add(key+".totalTimeUntilFirstWrite", rw.end.Sub(start).Nanoseconds())
+				metrics.Add(key+".totalTimeUntilFirstWrite", rw.firstWriteAt.Sub(start).Nanoseconds())
 				metrics.Add(key+".totalTimeInHandlers", end.Sub(start).Nanoseconds())
 			}
 		}
@@ -101,12 +116,13 @@ var _ Unwrapper = (*metricsResponseWriter)(nil)
 
 type metricsResponseWriter struct {
 	http.ResponseWriter
-	mu         sync.Mutex
-	rc         *http.ResponseController
-	keys       []string
-	metrics    *expvar.Map
-	statusCode int
-	end        time.Time
+	mu           sync.Mutex
+	rc           *http.ResponseController
+	keys         []string
+	metrics      *expvar.Map
+	statusCode   int
+	firstWriteAt time.Time
+	hijackedAt   time.Time
 }
 
 func (w *metricsResponseWriter) Unwrap() http.ResponseWriter {
@@ -114,13 +130,19 @@ func (w *metricsResponseWriter) Unwrap() http.ResponseWriter {
 }
 
 func (w *metricsResponseWriter) FlushError() error {
-	w.recordEnd()
+	w.recordFirstWrite()
 
 	return w.rc.Flush()
 }
 
+func (w *metricsResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	w.recordHijack()
+
+	return w.rc.Hijack()
+}
+
 func (w *metricsResponseWriter) Write(b []byte) (int, error) {
-	w.recordEnd()
+	w.recordFirstWrite()
 
 	for _, key := range w.keys {
 		w.metrics.Add(key+".totalBytesWritten", int64(len(b)))
@@ -132,18 +154,37 @@ func (w *metricsResponseWriter) Write(b []byte) (int, error) {
 func (w *metricsResponseWriter) WriteHeader(statusCode int) {
 	w.ResponseWriter.WriteHeader(statusCode)
 
-	w.recordEnd()
+	w.recordFirstWrite()
 
 	if w.statusCode == 0 {
 		w.statusCode = statusCode
 	}
 }
 
-func (w *metricsResponseWriter) recordEnd() {
+func (w *metricsResponseWriter) recordFirstWrite() {
 	w.mu.Lock()
 	defer w.mu.Unlock()
 
-	if w.end.IsZero() {
-		w.end = time.Now()
+	if w.firstWriteAt.IsZero() {
+		w.firstWriteAt = time.Now()
+	}
+}
+
+func (w *metricsResponseWriter) recordHijack() {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.hijackedAt.IsZero() {
+		w.hijackedAt = time.Now()
+	}
+
+	// If there is no first write time recorded yet then we just
+	// set it to be the same as the hijacked time
+	//
+	// This is because after the TCP connection has been hijacked
+	// it can still be written to, but we're only interedted in
+	// recording writes in the context of a normal HTTP handler here
+	if w.firstWriteAt.IsZero() {
+		w.firstWriteAt = w.hijackedAt
 	}
 }
