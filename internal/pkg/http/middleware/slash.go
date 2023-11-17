2abd9f64878ab4c00666b60f8306d7e8086eb64d
Polyscone <polyscone@gmail.com>
2023-11-17 17:21:21 +0900

    Fix remove slash middleware for sub routers

--- internal/pkg/http/middleware/slash.go
+++ internal/pkg/http/middleware/slash.go
@@ -7,7 +7,7 @@ import (
 
 func RemoveTrailingSlash(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
+		if r.URL.Path != "" && r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
 			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
 
 			http.Redirect(w, r, r.URL.String(), http.StatusMovedPermanently)
