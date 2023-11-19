f315bc61bc4f4fcf1fe9a8f2c3607ca117067796
Polyscone <polyscone@gmail.com>
2023-11-19 23:26:24 +0900

    Add a helper function for error messages

--- internal/adapter/web/httputil/error.go
+++ internal/adapter/web/httputil/error.go
@@ -74,3 +74,62 @@ func ErrorStatus(err error) int {
 
 	return http.StatusInternalServerError
 }
+
+func ErrorMessage(err error) string {
+	switch {
+	case errors.Is(err, ErrNotFound),
+		errors.Is(err, app.ErrNotFound):
+
+		return "The page you were looking for could not be found."
+
+	case errors.Is(err, ErrMethodNotAllowed):
+		return "Method not allowed."
+
+	case errors.Is(err, ErrForbidden),
+		errors.Is(err, app.ErrForbidden):
+
+		return "You do not have sufficient permissions to access this resource."
+
+	case errors.Is(err, http.ErrHandlerTimeout):
+		return "The server took too long to respond."
+
+	case errors.Is(err, account.ErrNotVerified):
+		return "This account is not verified."
+
+	case errors.Is(err, account.ErrNotActivated):
+		return "This account is not activated."
+
+	case errors.Is(err, account.ErrSuspended):
+		return "This account has been suspended."
+
+	case errors.Is(err, app.ErrUnauthorised):
+		return "You do not have permission to access this resource."
+
+	case errors.Is(err, app.ErrMalformedInput),
+		errors.Is(err, app.ErrInvalidInput),
+		errors.Is(err, app.ErrConflictingInput):
+
+		if errors.Is(err, app.ErrMalformedInput) {
+			return "Malformed input."
+		} else {
+			return "Invalid input."
+		}
+
+	case errors.Is(err, csrf.ErrEmptyToken):
+		return "Empty CSRF token."
+
+	case errors.Is(err, csrf.ErrInvalidToken):
+		return "Invalid CSRF token."
+
+	case errors.Is(err, rate.ErrInsufficientTokens),
+		errors.Is(err, account.ErrSignInThrottled):
+
+		return "You have made too many consecutive requests. Please try again later."
+
+	case errors.Is(err, repository.ErrLogin):
+		return "Could not connect to the datasource."
+
+	default:
+		return "An error has occurred."
+	}
+}
