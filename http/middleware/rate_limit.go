fd0876c9419ef37ef0e78981a2d7c9c8f6fd15c1
Polyscone <polyscone@gmail.com>
2024-05-02 13:12:23 +0900

    Set the `connection` header to `close` when a client hits the rate limit

--- http/middleware/rate_limit.go
+++ http/middleware/rate_limit.go
@@ -78,6 +78,20 @@ func RateLimit(capacity, replenish float64, config *RateLimitConfig) Middleware 
 				client := getClient(ip)
 
 				if _, err := client.bucket.Take(1, time.Now()); err != nil {
+					// If a client is hitting the rate limit we set the connection header to
+					// close which will trigger the standard library's HTTP server to close
+					// the connection after the response is sent
+					//
+					// Doing this means the client needs to go through the handshake process
+					// again to make a new connection the next time, which should help to slow
+					// down additional requests for clients that keep on hitting the limit
+					//
+					// This has no effect when headers have already been sent unless the HTTP
+					// status code was of the 1xx class or the modified headers are trailers, but
+					// this middleware should ideally be doing its checks before any handlers
+					// would be writing to the response writer anyway
+					w.Header().Set("connection", "close")
+
 					handleError(w, r, err, config.ErrorHandler, http.StatusTooManyRequests)
 
 					return
